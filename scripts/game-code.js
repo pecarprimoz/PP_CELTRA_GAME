610a0629c25dce47aaea00f7a9c3a9d223dd01a7
ppecar <pp1508@student.uni-lj.si>
2017-05-21 12:33:24 +0200

    Mobile support now fully implemented, fixed design, game now good 2go

--- scripts/game-code.js
+++ scripts/game-code.js
@@ -8,7 +8,15 @@ let canvas, ctx, width, height, player,
     player_hp=3,
     num_of_platforms=5,
     player_coins=0;
-
+let onphone=false;
+function checkIfRunningOnPhone(){
+    window.mobilecheck = function() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    };
+    return window.mobilecheck();
+}
 
 const keys = [];
 const map = [];
@@ -37,9 +45,11 @@ function restartCurrentlevel(){
     setHudParams();
 }
 const initGame = function(){
+    onphone=checkIfRunningOnPhone();
     initCanvas();
     initPlayer();
     initTiles();
+    initMobileControls()
     createTileMap();
     setHudParams();
     makeMapDynamic();
@@ -65,6 +75,7 @@ const initGame = function(){
 
         widthCols=tileSide;
         heightCols=Math.ceil(height/tileSide);
+
     }
 
     function createTileMap(){
@@ -347,12 +358,52 @@ function initEnemy(){
     return enemy;
 }
 
+function initMobileControls(){
+    document.getElementById("LEFT").addEventListener("touchstart", function () {
+        keys[420]=true
+    });
+    document.getElementById("LEFT").addEventListener("touchend", function () {
+        keys[420]=false
+    });
+    document.getElementById("RIGHT").addEventListener("touchstart", function () {
+        keys[421]=true
+    });
+    document.getElementById("RIGHT").addEventListener("touchend", function () {
+        keys[421]=false
+    });
+    document.getElementById("JUMP").addEventListener("touchstart", function () {
+        keys[422]=true
+    });
+    document.getElementById("JUMP").addEventListener("touchend", function () {
+        keys[422]=false
+    });
+    document.getElementById("ACTION").addEventListener("touchstart", function () {
+        keys[423]=true
+    });
+    document.getElementById("ACTION").addEventListener("touchend", function () {
+        keys[423]=false
+    });
+}
+function setPhoneControlsOff(){
+    document.getElementById("LEFT").style.display = "none";
+    document.getElementById("RIGHT").style.display = "none";
+    document.getElementById("JUMP").style.display = "none";
+    document.getElementById("ACTION").style.display = "none";
+}
 
 const controls = () =>{
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    ){
         keyboardControls()
     }
     else if(width>1000){
+        setPhoneControlsOff()
         keyboardControls()
     }
 
@@ -394,7 +445,7 @@ const controls = () =>{
                 player.y+=player.speed;
         }*/
         //desno
-        if (keys[39]) {
+        if (keys[39] ||keys[421]) {
             collisionDetectionSpecificRight();
             //console.log(player.x+player.width)
             if(player.x+player.width>(canvas.width/widthCols)-widthCols/4){
@@ -415,7 +466,7 @@ const controls = () =>{
         }
 
         //levo
-        if (keys[37]) {
+        if (keys[37] || keys[420]) {
             collisionDetectionSpecificLeft();
             if(player.x<widthCols/4){
                 if(tileOffsetX>=tileSide){
@@ -435,18 +486,97 @@ const controls = () =>{
 };
 let canBuild=true;
 function update(){
-
+    if(onphone){
+        mobileBasedGame()
+    }
+    else{
+        computerBasedGame()
+    }
     //preverjamo premikanje
     //console.log(player.y)
 
     controls();
+
+
+    //enemy.movement()
+
+
+    //izris na canvas
+    //collisionDetection()
+    draw();
+    checkifdied();
+
+    //suconsole.log(player.x,player.y)
+    requestAnimationFrame(update);
+
+}
+function mobileBasedGame(){
     if(collisionDetectionSpecificUp()){
         gravity+=0.3;
         player.y+=player.speed;
     }
 
     if(collisionDetectionSpecificDown()){
-    // console.log("done")
+        // console.log("done")
+        nextJumpPossible=false;
+        canBuild=false;
+        player.jumping=false;
+        player.numofjumps=powerJumps;
+        //kle mas neko foro z + ali - ce bo kej narobe
+        gravity=player.speed
+    }
+    if(!collisionDetectionSpecificDownDontChange() && !player.jumping){
+        player.jumping=true;
+        canBuild=true
+    }
+    //console.log(collisionDetectionSpecificDownDontChange())
+    if(!collisionDetectionSpecificDownDontChange() && player.numofjumps>0 && !keys[422]){
+        nextJumpPossible=true;
+        canBuild=true
+    }
+    if(player.jumping && nextJumpPossible && keys[422] && player.numofjumps>0){
+        canBuild=true
+        //console.log("test")
+        //console.log("?")
+        gravity=-0.5;
+        //console.log(player.numofjumps)
+        player.numofjumps--;
+        //console.log("jump")
+    }
+
+
+    if(collisionDetectionSpecificDownDontChange() && !player.jumping){
+        player.y=player.y-player.speed;
+        if(keys[422]){
+            canBuild=true
+            //console.log("fist")
+            gravity=-0.5;
+            player.jumping=true;
+            player.numofjumps--;
+            // console.log(player.numofjumps)
+            //console.log("jump")
+        }
+    }
+    if((keys[423]) && canBuild){
+        platformCreator();
+        canBuild=false
+    }
+
+    if(player.jumping ){
+        gravity+=0.03
+    }
+
+    player.y+=gravity
+}
+
+function computerBasedGame(){
+    if(collisionDetectionSpecificUp()){
+        gravity+=0.3;
+        player.y+=player.speed;
+    }
+
+    if(collisionDetectionSpecificDown()){
+        // console.log("done")
         nextJumpPossible=false;
         canBuild=false;
         player.jumping=false;
@@ -482,11 +612,11 @@ function update(){
             gravity=-0.5;
             player.jumping=true;
             player.numofjumps--;
-           // console.log(player.numofjumps)
+            // console.log(player.numofjumps)
             //console.log("jump")
         }
     }
-    if(keys[32] && canBuild){
+    if((keys[32]) && canBuild){
         platformCreator();
         canBuild=false
     }
@@ -496,18 +626,6 @@ function update(){
     }
 
     player.y+=gravity
-
-    //enemy.movement()
-
-
-    //izris na canvas
-    //collisionDetection()
-    draw();
-    checkifdied();
-
-    //suconsole.log(player.x,player.y)
-    requestAnimationFrame(update);
-
 }
 
 
