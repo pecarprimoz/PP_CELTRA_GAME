61bed27e1439992c8e9b2e1186b511d28918f256
pecarprimoz <pp1508@student.uni-lj.si>
2017-05-22 12:17:47 +0200

    Dodal nov feature

--- scripts/game-code.js
+++ scripts/game-code.js
@@ -476,6 +476,15 @@ function initMobileControls(){
     document.getElementById("ACTION").addEventListener("touchend", function () {
         keys[423]=false
     },{passive:true});
+    canvas.addEventListener("touchstart", function (e) {
+        let posX = Math.ceil(worldOffsetX+e.touches[0].pageX/tileSide) - Math.floor(tileOffsetX/tileSide+0.25);
+        let posY = Math.ceil(e.touches[0].pageY/tileSide + tileOffsetY+0.25)
+        if(map[posY][posX]===1){
+            map[posY][posX]=0;
+        }
+    },{passive:true});
+    canvas.addEventListener("touchend", function () {
+    },{passive:true});
 }
 
 //V primeru da nismo na telefonu pobrišemo dodatne hud elemente
@@ -486,10 +495,14 @@ function setPhoneControlsOff(){
     document.getElementById("ACTION").style.display = "none";
 }
 
+let initCont=false;
 //Izbira kontrol in nastavitev načina igre
 const controls = () =>{
     if(onphone){
-        initMobileControls();
+        if(!initCont){
+            initMobileControls();
+            initCont=true;
+        }
         keyboardControls();
     }
     else if(width>1000){
